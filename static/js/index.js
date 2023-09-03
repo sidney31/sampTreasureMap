9e1a82df6e94329f1edf866d71513eafe776415c
Богдан <100060433+sidney31@users.noreply.github.com>
2023-09-03 21:19:22 +0300

    fix map exit beyond  screen at zoom: 1;

--- static/js/index.js
+++ static/js/index.js
@@ -3,12 +3,10 @@ try {
         const elem = document.querySelector('#map-container')
         const parent = elem.parentElement
         const panzoom = Panzoom(elem, {
-            maxScale: 3,
-            minScale: 0.2,
+            maxScale: 4,
+            minScale: 1,
             cursor: "url(\"../img/cursor.cur\")",
             contain: "outside",
-            startScale: 0.2,
-            smoothScroll: false
         })
         parent.addEventListener('wheel', panzoom.zoomWithWheel)
     });
@@ -27,35 +25,12 @@ try {
     console.log(e)
 }
 
-
-function copyToClipboard(text){
-    if (window.isSecureContext && navigator.clipboard) {
-        navigator.clipboard.writeText(text);
-    } else {
-        unsecuredCopyToClipboard(text);
-    }
-}
-
-function unsecuredCopyToClipboard(text) {
-    const textArea = document.createElement("textarea");
-    textArea.value = text;
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-    try {
-        document.execCommand('copy')
-    } catch (err) {
-        console.error('Unable to copy to clipboard', err)
-    }
-    document.body.removeChild(textArea)
-}
-
 try {
     let notification = document.querySelector("#notification")
     const answers = document.querySelectorAll('.answer')
     for (let i in answers) {
         answers[i].addEventListener('click', _ => {
-            copyToClipboard(answers[i].innerHTML)
+            navigator.clipboard.writeText(answers[i].innerHTML)
             ShowNotification(notification)
         }, false)
     }
