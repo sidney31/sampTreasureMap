3ce73d624bda2b623790b3364d5611c45301853c
Богдан <100060433+sidney31@users.noreply.github.com>
2023-09-06 13:11:32 +0300

    edit context

--- TreasureMap/views.py
+++ TreasureMap/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def index(request):
     context = {
         'title': 'Карта кладов',
-        'script': '{% static \'js/index.js\' %}',
+        'script': '{% static \'js/pan&zoom.js\' %}',
     }
     return render(request, 'map.html', context)
