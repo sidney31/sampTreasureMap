25352556564d48e581dc2ec3d4cf6a3f66804881
Богдан <100060433+sidney31@users.noreply.github.com>
2023-09-06 13:10:45 +0300

    add live-search to page

---
+++ static/js/live-search.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    document.body.style.overflowY = 'visible';
+    let search_box = document.querySelector(".search-box");
+    let allQuestionsBlocks = document.querySelectorAll(".question_block");
+    let questions_and_answers = document.querySelector("#questions_and_answers");
+    let stockAllQuestionsBlocks = questions_and_answers;
+
+    updateList();
+
+    search_box.onkeydown = function () {
+        updateList();
+        let search_text = this.value;
+        questions_and_answers.innerHTML = ''
+        allQuestionsBlocks.forEach((item, i) => {
+            if (item.querySelector(".question").textContent.toLowerCase().includes(search_text.toLowerCase())) {
+                questions_and_answers.innerHTML +=
+                    `
+                    <div class="question_block">
+                        <div class="question">${item.querySelector(".question").textContent}</div>
+                        <div class="answer">${item.querySelector(".answer").textContent}</div>
+                    </div>
+                    `
+            }
+        })
+
+    }
+
+    function updateList() {
+        questions_and_answers = stockAllQuestionsBlocks
+    }
+}
